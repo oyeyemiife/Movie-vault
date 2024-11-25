722604d7159cbadb3fbae76bb7aa46415cbbdd74
Ifeoluwa Oyeyemi <ifeoluwaoyeyemi915@gmail.com>
2024-11-25 10:50:11 +0100

    modified

--- Movievault/src/pages/LandingPage/Landingpage.tsx
+++ Movievault/src/pages/LandingPage/Landingpage.tsx
@@ -1,6 +1,6 @@
 import { Moviecard } from '../MovieCard/Moviecard';
 import './landingpage.css' 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { fetchTrendingMovies } from './tmdbservice';
 import { Layout } from '../../components/Dashboard/layout';
 import background from '../../assets/Navbar/back.webp'
@@ -19,7 +19,37 @@ interface MovieWithGenres {
 export const Landingpage = () => {
 
     const [movies, setMovies] = useState<MovieWithGenres[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState<string | null>(null);
 
+    const genreContainerRef = useRef<HTMLDivElement>(null);
+
+const scrollCategory = (scrollOffset: number) => {
+  if (genreContainerRef.current) {
+    genreContainerRef.current.scrollBy({
+      left: scrollOffset,
+      behavior: 'smooth',
+    });
+  }
+};
+
+    const genres = [
+      'Action',
+      'Adventure',
+      'Animation',
+      'Comedy',
+      'Crime',
+      'Documentary',
+      'Drama',
+      'Family',
+      'Fantasy',
+      'History',
+      'Horror',
+      'Mystery',
+      'Romance',
+      'Science Fiction',
+      'Thriller',
+      'Western'
+    ];
   useEffect(() => {
     const getMovies = async () => {
       const moviesData = await fetchTrendingMovies();
@@ -34,43 +64,41 @@ export const Landingpage = () => {
     alert(`You clicked on ${title}`);
   };
 
+  const filteredMovies = selectedGenre
+    ? movies.filter(movie => movie.genreNames.includes(selectedGenre))
+    : movies;
+
   return (
     <Layout headerText="Your Gateway to Unlimited Cinema Magic!" headerBg={background}>
         <div className='homecontainer'>
             <div className='genre'>
-            {[ 'Action',
-                'Adventure',
-                'Animation',
-                'Comedy',
-                'Crime',
-                'Documentary',
-                'Drama',
-                'Family',
-                'Fantasy',
-                'History',
-                'Horror',
-                'Mystery',
-                'Romance',
-                'Science Fiction',
-                'Thriller',
-                'Western'
-            ].map((category) => (
-            <div key={category} className="recommendation">
-              <p>{category}</p>
-            </div>
-          ))}
-            </div>
+            <button type='submit' title='scroll' className="scroll-button left" onClick={() => scrollCategory(-600)}>
+        &#8249;
+      </button>
+      <div className="recommendation-container" ref={genreContainerRef}>
+                {genres.map((category) => (
+              <div key={category} className={`recommendation ${selectedGenre===category? 'active' : ''}`}
+              onClick={() => setSelectedGenre(category === selectedGenre ? null : category)}
+              >
+                  <p>{category}</p>
+                </div>
+              ))}
+                </div>
+                <button type='submit' title='scroll' className="scroll-button right" onClick={() => scrollCategory(600)}>
+        &#8250;
+      </button>
+      </div>
         <div className='movies'>
-      {movies.map((movie:any) => (
-        <Moviecard
-          key={movie.id}
-          title={movie.title}
-          posterUrl={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          year={movie.release_date ? movie.release_date.split('-')[0] : 'N/A'}
-          rating={movie.vote_average}
-          genre={movie.genreNames}
-          onClick={() => handleCardClick(movie.title)}
-        />
+        {filteredMovies.map((movie:MovieWithGenres) => (
+          <Moviecard
+            key={movie.id}
+            title={movie.title}
+            posterUrl={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            year={movie.release_date ? movie.release_date.split('-')[0] : 'N/A'}
+            rating={movie.vote_average}
+            genre={movie.genreNames}
+            onClick={() => handleCardClick(movie.title)}
+          />
       ))}
       </div>
     </div>
